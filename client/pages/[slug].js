0aa6d8551391b5b33a3a7ee57c9ba194a0e0d3e2
Lucas Inocente <olucassanchez@gmail.com>
2021-08-01 22:06:40 -0300

    Atualiza layout da listagem de episódios da página de podcast

--- client/pages/[slug].js
+++ client/pages/[slug].js
@@ -26,23 +26,47 @@ export default function PodcastPage({ slug, json }) {
       <Container>
         <Title text={title} />
         <SubTitle text={description} />
-      </Container>
-      {
-        episodes.map((data) => {
-          const title = data.title._cdata;
-          const audio = data.enclosure._attributes.url;
+        <SubTitle text={'Episodes:'} />
+        {
+          episodes.map((data) => {
+            const title = data.title._cdata;
+            const pubDate = data.pubDate._text;
+            const creator = data['dc:creator']._cdata;
+            const audio = data.enclosure._attributes.url;
+            const image = data['itunes:image']._attributes.href;
+            const description = data['itunes:summary']._text;
 
-          return (
-            <Card key={title}>
-              <h2>{ title }</h2>
-              <audio controls>
-                <source src={audio} type="audio/x-m4a" />
-              </audio>
-            </Card>
-          )
-        })
-      }
-      <Footer />
+            return (
+              <Card key={title}>
+                <div className="card-episode">
+                  <div className="card-episode-header">
+                    <div>
+                      <img src={image} height="144" />
+                    </div>
+                    <div className="card-episode-metadata">
+                      <div>
+                        <h2>{ title }</h2>
+                        <p><strong>{creator}</strong> - { pubDate }</p>
+                      </div>
+                      <audio controls>
+                        <source src={audio} type="audio/x-m4a" />
+                      </audio>
+                    </div>
+                  </div>
+                  <hr className="spacer" />
+                  <h3>Description:</h3>
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html: description
+                    }}>
+                  </div>
+                </div>
+              </Card>
+            )
+          })
+        }
+        <Footer />
+      </Container>
     </>
   )
 }
